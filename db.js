34cce63131fa951ad2b124d32d59cb8eeb4838c2
jaya-myhq <122013950+jaya-myhq@users.noreply.github.com>
2025-07-07 23:28:12 +0530

    updated with my mongo db instance (google sing up)

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://kuwarjain394:VNgeX7TNT1MV4BAm@properties.dn1a7zn.mongodb.net/?retryWrites=true&w=majority&appName=Properties", {
+mongoose.connect("mongodb+srv://jayamukesh:KUudX5ZtsrQz0gM6@cluster0.s5y4hk8.mongodb.net/?retryWrites=true&w=majority&appName=Properties", {
 })
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
